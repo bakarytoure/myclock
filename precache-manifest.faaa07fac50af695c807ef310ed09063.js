3b7879962e9935d8edb6bcf17b6c7ee8b191dc16
bakarytoure <bakarytoure57@gmail.com>
2020-07-09 14:38:42 +0000

    Updates

---
+++ precache-manifest.faaa07fac50af695c807ef310ed09063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795d77256fcfe26b32f657600375b3c3",
+    "url": "/myclock/index.html"
+  },
+  {
+    "revision": "d2b5a8e3c50d7196906a",
+    "url": "/myclock/static/css/main.8ad02eb7.chunk.css"
+  },
+  {
+    "revision": "bdb6713ad626a615777e",
+    "url": "/myclock/static/js/2.be0c0d2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myclock/static/js/2.be0c0d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b5a8e3c50d7196906a",
+    "url": "/myclock/static/js/main.33db5d64.chunk.js"
+  },
+  {
+    "revision": "981aa807e295d01a7d9b",
+    "url": "/myclock/static/js/runtime-main.cd04ef9c.js"
+  }
+]);
